c787a2e7c48679692c7b4000a0d33d9737facd54
65160043 <157364889+65160043@users.noreply.github.com>
2024-01-22 18:39:03 +0700

    Create calculate.py

---
+++ calculate.py
@@ -0,0 +1,15 @@
+
+number1 =float(input("ป้อนตัวลเขที่1: "))
+number2 =float(input("ป้อนตัวลเขที่2: "))
+
+sum_result = number1 + number2
+print(f"ผลบวก: {sum_rusult}")
+
+sum_result = number1 - number2
+print(f"ผลลบ: {sum_rusult}")
+
+sum_result = number1 * number2
+print(f"ผลคูณ: {sum_rusult}")
+
+sum_result = number1 / number2
+print(f"ผลหาร: {sum_rusult}")
